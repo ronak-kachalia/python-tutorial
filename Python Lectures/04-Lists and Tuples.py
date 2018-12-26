37e35be6a6fd147984b9150b12bfa08d8e940516
Ronak <ronak@Ronak.local>
2018-12-26 10:56:07 +0530

    Initial Commit

--- Python Lectures/04-Lists and Tuples.py
+++ Python Lectures/04-Lists and Tuples.py
@@ -19,13 +19,17 @@ Indexing and slicing work just like in strings
 '''
 #%%
 # Basic List operations
-my_list.append()
+my_list.append('seven')
 my_list.pop()
-my_list.pop(1)
+my_list.pop(1) # index
 my_list.sort()
 my_list.reverse()
 my_list.remove()
-my_list.remove('two')
+my_list.remove('two') # value
+other_list = my_list.copy() # new copy with different reference
+other_list.append('eight') # only other_list changes
+print(other_list)
+print(my_list)
 #%%
 # Tuples
 '''
@@ -33,6 +37,8 @@ Tuples are similar to Lists but immutable
 '''
 #%%
 t = (1,2,3,'four')
+t.count('four')
+t.index('four')
 #%%
 # List Comprehensions
 '''
@@ -53,7 +59,7 @@ first_col = [row[0] for row in matrix]
 lst = [x for x in 'word']
 lst
 #%%
-lst = [x**2 for x in range(0,11)]
+lst = [x**2 for x in range(11)]
 lst
 #%%
 lst = [x for x in range(11) if x % 2 == 0]
